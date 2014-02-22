e3d394cc13b1cd191693a0e37841f7becf8870e5
Stefan Saasen <ssaasen@atlassian.com>
2014-02-22 12:16:51 +1100

    Test: Remove unused imports

--- tests/src/Handler/RestTests.hs
+++ tests/src/Handler/RestTests.hs
@@ -3,24 +3,12 @@ module Handler.RestTests
   (
     main
   , defaultTestGroup
-  ) where
+) where
 
-import qualified Test.HUnit
-import qualified Data.Text              as T
-import qualified Data.ByteString.Char8  as B
 import           Test.Tasty
 import           Test.Tasty.TH
-import           Test.Tasty.QuickCheck  as QC
 import           Test.Tasty.HUnit
-import           Test.QuickCheck hiding (Success)
-import           Control.Applicative
-import           Control.Monad          (mzero)
-import           Data.Bits
-import           Data.Word
 import qualified Data.Map.Lazy as Map
-import           Data.Aeson.Encode
-import           Data.Aeson.Parser      (value)
-import           Data.Aeson.Types
 
 import           Snap.Core hiding (setHeader)
 import           Snap.Test
--- tests/src/TestRunner.hs
+++ tests/src/TestRunner.hs
@@ -2,8 +2,6 @@ module Main where
 
 import qualified Handler.RestTests
 import           Test.Tasty
-import           Test.Tasty.QuickCheck as QC
-import           Test.Tasty.HUnit
 
 main :: IO ()
 main = defaultMain tests
