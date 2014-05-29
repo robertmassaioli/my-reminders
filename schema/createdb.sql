7fe8aef4ea5a78fa8ff23be1f4f5c42c56b278e9
Eero Kaukonen <eero.kaukonen@gmail.com>
2014-05-29 11:24:45 +1000

    adding database cretion scripts

---
+++ schema/createdb.sql
@@ -0,0 +1,16 @@
+﻿-- Database: pingme
+
+DROP TABLE IF EXISTS tenant;
+
+CREATE TABLE tenant (
+     id  SERIAL PRIMARY KEY
+  ,  key VARCHAR(255) unique not null
+  ,  publicKey TEXT not null
+  ,  sharedSecret VARCHAR(512) null
+  ,  baseUrl VARCHAR(512) unique not null
+  ,  productType VARCHAR(50) not null
+);
+
+DROP INDEX IF EXISTS tenant_idx;
+
+CREATE INDEX tenant_idx ON tenant (sharedSecret);
