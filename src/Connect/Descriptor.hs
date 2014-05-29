eb33add1adfb3dc0e65d49582d27e62904cfa890
Eero Kaukonen <eero.kaukonen@gmail.com>
2014-05-29 10:19:10 +1000

    adding descriptor stuff

---
+++ src/Connect/Descriptor.hs
@@ -0,0 +1,137 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, StandaloneDeriving #-}
+package Connect
+
+module Descriptor where
+
+import Control.Monad
+import Data.Aeson
+import Data.Aeson.Types
+import qualified Data.Char as C
+import Data.Text
+import Data.Maybe
+import qualified Data.List as L
+import GHC.Generics
+import Network.URI
+
+data Plugin = Plugin
+    { pluginName :: Maybe Text
+    , pluginDescription :: Maybe Text
+    , pluginKey :: Text
+    , pluginBaseUrl :: URI
+    , vendor :: Maybe Vendor
+    , authentication :: Authentication
+    , apiVersion :: Maybe Text
+    , modules :: Maybe Modules
+    , enableLicensing :: Maybe Bool
+    , lifecycle :: Maybe Lifecycle
+    , links :: Maybe [(Text, URI)]
+    , scopes :: Maybe [Text]
+    } deriving (Show, Generic)
+
+
+data Vendor = Vendor {vendorName :: Text, vendorUrl :: URI} deriving (Show, Generic)
+
+data Authentication = Authentication {authType :: AuthType, publicKey :: Maybe Text} deriving (Show, Generic)
+
+data AuthType = OAuth | Jwt | None deriving (Show, Generic)
+
+data Modules = Modules JiraModules deriving (Show, Generic) -- TODO
+
+data JiraModules = JiraModules 
+    { webPanels :: [WebPanel]
+    } deriving (Show, Generic)
+
+data WebPanel = WebPanel
+    { key :: Text
+    , name :: NameValue
+    , url :: Text
+    , location :: Text
+    } deriving (Show, Generic)
+
+data NameValue = NameValue 
+    { value :: Text
+    } deriving (Show, Generic)
+        
+data Lifecycle = Lifecycle
+    { installed :: Maybe URI
+    , uninstalled :: Maybe URI
+    , enabled :: Maybe URI
+    , disabled :: Maybe URI
+    } deriving (Show, Generic) -- TODO
+
+instance ToJSON Plugin where
+    toJSON = genericToJSON baseOptions
+        { fieldLabelModifier = stripFieldNamePrefix "plugin"
+        }
+
+instance ToJSON Vendor where
+    toJSON = genericToJSON baseOptions
+        { fieldLabelModifier = stripFieldNamePrefix "vendor"
+        }
+
+instance ToJSON Authentication where
+    toJSON = genericToJSON baseOptions
+        { fieldLabelModifier = stripFieldNamePrefix "auth"
+        }
+
+instance ToJSON AuthType where
+    toJSON OAuth = "oauth"
+    toJSON Jwt   = "jwt"
+    toJSON None  = "none"
+
+instance ToJSON URI where
+    toJSON = String . pack . show
+
+instance ToJSON Lifecycle where
+    toJSON = genericToJSON baseOptions
+
+instance ToJSON Modules where
+    toJSON = genericToJSON baseOptions
+
+instance ToJSON JiraModules where
+    toJSON = genericToJSON baseOptions
+
+instance ToJSON WebPanel where
+    toJSON = genericToJSON baseOptions
+    
+instance ToJSON NameValue where
+    toJSON = genericToJSON baseOptions
+        
+instance FromJSON URI where
+    parseJSON (String uriString) = maybe mzero return (parseURI $ unpack uriString)
+    parseJSON _ = mzero
+
+
+baseOptions :: Options
+baseOptions = defaultOptions
+    { omitNothingFields = True
+    }
+
+stripFieldNamePrefix :: String -> String -> String
+stripFieldNamePrefix pre s = toLowerFirst $ fromMaybe s (L.stripPrefix pre s)
+    where toLowerFirst (c : cs) = C.toLower c : cs
+          toLowerFirst [] = []
+
+defaultLifecycle :: Lifecycle
+defaultLifecycle = Lifecycle
+    { installed = parseURI "/installed"
+    , uninstalled = parseURI "/uninstalled"
+    , enabled = parseURI "/enabled"
+    , disabled = parseURI "/disabled"
+    }
+
+pluginDescriptor :: Text -> URI -> Authentication -> Plugin
+pluginDescriptor key url auth = Plugin
+    { pluginName = Nothing
+    , pluginDescription = Nothing
+    , pluginKey = key
+    , pluginBaseUrl = url
+    , vendor = Nothing
+    , authentication = auth
+    , apiVersion = Nothing
+    , modules = Nothing
+    , enableLicensing = Nothing
+    , lifecycle = Nothing
+    , links = Nothing
+    , scopes = Nothing
+    }
