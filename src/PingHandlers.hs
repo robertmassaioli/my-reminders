10880951f225e3ee2ba5e84ecca7fd5520c8ce12
Robert Massaioli <robertmassaioli@gmail.com>
2014-05-31 00:30:09 +1000

    Finished refactoring out the code to make it much easier to read.

--- src/PingHandlers.hs
+++ src/PingHandlers.hs
@@ -6,6 +6,7 @@
 module PingHandlers(addPingHandler, executePingsHandler) where
 
 import qualified Data.Text as T 
+import qualified Data.ByteString.Char8 as BSC
 import Snap.Core
 import Snap.Snaplet
 import Data.Aeson
@@ -13,13 +14,11 @@ import Data.Maybe
 import Application
 import Data.Time.Clock
 import Data.Time.Clock.POSIX
-import Snap.Snaplet.PostgresqlSimple
 import Database.PostgreSQL.Simple
 import Persistence.PostgreSQL
 import qualified Persistence.Ping as P 
 import GHC.Generics
 import Network.URI
-import Control.Monad
 import Control.Monad.IO.Class
 
 data PingRequest = PingRequest {
@@ -37,26 +36,51 @@ instance ToJSON PingRequest
 --TODO: this needs to be authenticated
 addPingHandler:: AppHandler ()
 addPingHandler = do
-   request <- readRequestBody (1024 * 10)
+   request <- readRequestBody (1024 * 10) -- TODO this magic number is crappy, improve
    let maybePing = Data.Aeson.decode request :: Maybe PingRequest
    maybe respondBadRequest addPing maybePing
-   where
-      respondBadRequest = modifyResponse . setResponseCode $ 400
 
-      addPing :: PingRequest -> AppHandler ()
-      addPing ping = do
-         let link' = fromMaybe nullURI $ parseURI (issueLink ping)                                             
-         let date' = posixSecondsToUTCTime $ (realToFrac $ timeStamp ping) / 1000
-         addedPing <- with db $ withConnection $ 
-            \conn -> P.addPing conn date' (tenantId ping) link' (userID ping) (message ping)  
-         case addedPing of
-            Just _ -> modifyResponse $ setResponseCode 204
-            Nothing -> do
-               logError "Failed to insert new tenant"
-               modifyResponse $ setResponseCode 500
+respondWith :: MonadSnap m => Int -> m ()
+respondWith = modifyResponse . setResponseCode
+
+respondBadRequest       :: MonadSnap m => m ()
+respondInternalServer   :: MonadSnap m => m ()
+respondNoContent        :: MonadSnap m => m ()
+respondBadRequest       = respondWith 400
+respondInternalServer   = respondWith 500
+respondNoContent        = respondWith 204
+
+addPing :: PingRequest -> AppHandler ()
+addPing pingRequest = do
+   addedPing <- with db $ withConnection (addPingFromRequest pingRequest)
+   case addedPing of
+      Just _ -> respondNoContent
+      Nothing -> do
+         logError . textToByteString $ failedInsertPrefix `T.append` userID pingRequest
+         respondInternalServer
+   where
+      failedInsertPrefix = T.pack "Failed to insert new ping: "
+
+textToByteString :: T.Text -> BSC.ByteString
+textToByteString = BSC.pack . T.unpack
+
+addPingFromRequest :: PingRequest -> Connection -> IO (Maybe Integer)
+addPingFromRequest pingRequest conn = 
+   P.addPing conn date' tenantId' link' userID' message'
+   where
+      date' = pingUTCTime pingRequest
+      tenantId' = tenantId pingRequest
+      link' = fromMaybe nullURI $ parseURI (issueLink pingRequest)
+      userID' = userID pingRequest
+      message' = message pingRequest
+
+pingUTCTime :: PingRequest -> UTCTime
+pingUTCTime pingRequest = posixSecondsToUTCTime posixSeconds
+   where
+      posixSeconds = (realToFrac . timeStamp $ pingRequest) / 1000.0
 
 --Returns the pingID if ping was a success
-ping:: P.Ping -> IO(Maybe Integer)
+ping :: P.Ping -> IO(Maybe Integer)
 ping rq = do
   putStrLn("pinged: " ++ show rq)
   return $ Just (P.pingId rq)
