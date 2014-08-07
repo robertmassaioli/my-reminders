97b6f3039afd27b1ec5565fc83227441a550c033
Avi Knoll <aknoll@atlassian.com>
2014-08-07 01:31:10 +0000

    CHA-26: Starting to set up Snaplet tests

--- tests/PingHandlersTests.hs
+++ tests/PingHandlersTests.hs
@@ -1,9 +1,13 @@
 module PingHandlersTests where
 
+import Data.ByteString.Char8 as C
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
+import Snap.Test hiding (evalHandler, runHandler)
+import Snap.Snaplet.Test
 
 import PingHandlers (handlePings, handleMultiPings)
+import Site (app)
 
 prop_trivial :: Bool
 prop_trivial = True
@@ -11,3 +15,7 @@ prop_trivial = True
 tests =
   [ testProperty "trivial" prop_trivial
   ]
+
+prop_postFails = do
+  handler <- evalHandler Nothing (postRaw C.empty C.empty C.empty) handlePings app
+
