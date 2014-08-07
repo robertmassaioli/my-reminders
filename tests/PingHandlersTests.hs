66d3c5e6670002dcd29d0ca3685eb95ac281227b
Avi Knoll <aknoll@atlassian.com>
2014-08-07 13:50:03 +1000

    CHA-26: Working Snaplet test

--- tests/PingHandlersTests.hs
+++ tests/PingHandlersTests.hs
@@ -1,8 +1,10 @@
 module PingHandlersTests where
 
 import Data.ByteString.Char8 as C
+import Test.Framework
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
+import Test.QuickCheck.Monadic (assert, monadicIO)
 import Snap.Test hiding (evalHandler, runHandler)
 import Snap.Snaplet.Test
 
@@ -12,10 +14,12 @@ import Site (app)
 prop_trivial :: Bool
 prop_trivial = True
 
+tests :: [Test]
 tests =
   [ testProperty "trivial" prop_trivial
+  , testProperty "also trivial" prop_postFails
   ]
 
-prop_postFails = do
-  handler <- evalHandler Nothing (postRaw C.empty C.empty C.empty) handlePings app
-
+prop_postFails = monadicIO $ do
+  errorOrResponse <- runHandler Nothing (postRaw C.empty C.empty C.empty) handlePings app
+  assert True
