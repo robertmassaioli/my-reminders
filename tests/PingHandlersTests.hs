15daab10737a695dec042ddedd0473fcbb6b7639
Avi Knoll <aknoll@atlassian.com>
2014-08-07 16:56:59 +1000

    CHA-26: Test for unauthorised requests

--- tests/PingHandlersTests.hs
+++ tests/PingHandlersTests.hs
@@ -1,10 +1,14 @@
 module PingHandlersTests where
 
-import Data.ByteString.Char8 as C
+import Control.Monad.IO.Class
+import qualified Data.ByteString.Char8 as C
+import qualified Data.Map as M
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
-import Test.QuickCheck.Monadic (assert, monadicIO)
+import Test.QuickCheck.Instances
+import Test.QuickCheck.Monadic (assert, monadicIO, PropertyM)
+import Snap.Core hiding (setHeader)
 import Snap.Test hiding (evalHandler, runHandler)
 import Snap.Snaplet.Test
 
@@ -12,17 +16,40 @@ import PingHandlers (handlePings, handleMultiPings)
 import Site (app)
 import SnapHelpers
 import TestHelpers
-
-prop_trivial :: Bool
-prop_trivial = True
+import WithToken
 
 tests :: [Test]
-tests =
-  [ testProperty "trivial" prop_trivial
-  , testProperty "also trivial" prop_postFails
+-- Generation for these tests is currently irrelevant and Snaplet testing is slow,
+-- so we are keeping the generated test count low.
+tests = map (plusTestOptions $ emptyTestOptions {topt_maximum_generated_tests = Just 5})
+  [ testProperty "POST always fails with 404 Not found" prop_postFails
+  , testProperty "GET fails without valid authorisation with 401 Unauthorized" prop_unauthorisedGetFails
+  , testProperty "PUT fails without valid authorisation with 401 Unauthorized" prop_unauthorisedPutFails
+  , testProperty "DELETE fails without valid authorisation with 401 Unauthorized" prop_unauthorisedDeleteFails
   ]
 
 prop_postFails :: C.ByteString -> C.ByteString -> C.ByteString -> Property
 prop_postFails path contentType body = monadicIO $ do
   errorOrResponse <- runHandler Nothing (postRaw path contentType body) handlePings app
-  assert $ responseCodeIs notFound errorOrResponse
+  return $ responseCodeIs notFound errorOrResponse
+
+prop_unauthorisedGetFails :: Params -> Property
+prop_unauthorisedGetFails params = requestIsUnauthorised (do
+  get emptyPath params
+  uncurry setHeader emptyPageTokenHeader)
+
+prop_unauthorisedPutFails :: C.ByteString -> C.ByteString -> Property
+prop_unauthorisedPutFails ct body = requestIsUnauthorised (do
+  put emptyPath ct body
+  uncurry setHeader emptyPageTokenHeader)
+
+prop_unauthorisedDeleteFails :: Params -> Property
+prop_unauthorisedDeleteFails params = requestIsUnauthorised (do
+  delete emptyPath params
+  uncurry setHeader emptyPageTokenHeader)
+
+requestIsUnauthorised :: RequestBuilder (PropertyM IO) () -> Property
+requestIsUnauthorised req = monadicIO $ do
+  errorOrResponse <- runHandler Nothing req handlePings app
+  return $ responseCodeIs unauthorised errorOrResponse
+
--- tests/TestHelpers.hs
+++ tests/TestHelpers.hs
@@ -1,15 +1,27 @@
 module TestHelpers where
 
 import Data.ByteString.Char8 as C
-import Data.Text
+import Data.CaseInsensitive (CI)
+import Data.Monoid (mempty)
+import Data.Text as T
 import Network.URI
 import Snap.Core
 import Test.QuickCheck
+import Test.Framework (TestOptions)
 
 import SnapHelpers
+import WithToken
 
-responseCodeIs :: Int -> Either Text Response -> Bool
-responseCodeIs code errorOrRsp = either (\_ -> False) ((== code) . rspStatus) errorOrRsp
+responseCodeIs :: Int -> Either Text Response -> Property
+responseCodeIs code errorOrRsp = either
+  (\t -> counterexample (T.unpack t) False)
+  (property . (== code) . rspStatus)
+  errorOrRsp
 
-instance Arbitrary C.ByteString where
-  arbitrary = fmap C.pack arbitrary
+emptyPath :: C.ByteString
+emptyPath = C.empty
+
+emptyPageTokenHeader :: (CI C.ByteString, C.ByteString)
+emptyPageTokenHeader = (acHeaderName, C.empty)
+
+emptyTestOptions = mempty :: TestOptions
