e57b0b826ad1319c2a3696c4f5a440867dde7a51
Robert Massaioli <rmassaioli@atlassian.com>
2014-09-30 15:31:23 +1000

    CHA-38: Make the to-production.bash script behave correctly.
    Now we can build docker containers locally. The only step left now is to update the readme file so
    that people can run docker locally and get this working successfully.

--- to-production.bash
+++ to-production.bash
@@ -3,17 +3,37 @@
 # The purpose of this script is to build the production ready docker container and make sure that it
 # works.
 
+set -e
+echo "## Will fail immediately on error..."
+
 # TODO make sure that boot2docker has been run on linux
 # TODO setup the proper docker config
-mkdir -p production/to-copy
-cp -R resources production/to-copy
-cd production
+COPY_DIR="production/to-copy"
+rm -fr "$COPY_DIR"
+mkdir -p "$COPY_DIR"
+cp -R resources "$COPY_DIR"
 
 echo "## Getting the required dependencies out of the build..."
-CONTAINER_ID=`docker ps -lq`
-time docker cp "$CONTAINER_ID:/home/haskell/build/.cabal-sandbox/bin/ping-me-connect" to-copy
+if [ "x$CONTAINER_ID" == "x" ]
+then
+   CONTAINER_ID=`docker ps -lq`
+   echo "## Could not find container id. Using last container: $CONTAINER_ID"
+fi
+
+echo "## Searching for container: $CONTAINER_ID"
+if docker ps -aq | grep "$CONTAINER_ID" > /dev/null
+then
+   echo "## Using container: $CONTAINER_ID"
+else
+   echo "## Failed to find container '$CONTAINER_ID'. Quitting"
+   exit 1
+fi
+
+time docker cp "$CONTAINER_ID:/home/haskell/build/.cabal-sandbox/bin/ping-me-connect" "$COPY_DIR"
 
 echo "## Building the production Docker image."
+cd production
 time docker build .
 
 echo "## Finished successfully"
+set +e
