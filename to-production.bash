657aa9e439a71d5ca2fa61fdbfe1e7ee1834961b
Robert Massaioli <rmassaioli@atlassian.com>
2014-10-27 13:02:09 +1100

    CHA-50: Updated the docker image so that we provide the static resources too!

--- to-production.bash
+++ to-production.bash
@@ -25,6 +25,9 @@ COPY_DIR="production/to-copy"
 rm -fr "$COPY_DIR"
 mkdir -p "$COPY_DIR"
 cp -R resources "$COPY_DIR"
+cp -R static "$COPY_DIR"
+r.js -o build.js
+cp -R static-js "$COPY_DIR"
 
 echo "## Getting the required dependencies out of the build..."
 if [ "x$CONTAINER_ID" == "x" ]
