ce5258ba6d137b206704c89b1b0b7962c1516f2c
Robert Massaioli <rmassaioli@atlassian.com>
2014-10-29 09:58:28 +1100

    CHA-55: Wrote a script to load test the database.
    Now we can rapidly generate data for the database for testing purposes.

---
+++ create-test-data.bash
@@ -0,0 +1,32 @@
+TENANT_COUNT=100
+REMINDERS_PER_TENANT=1000
+
+PSQL="bash run-psql.bash"
+
+CREATE_FILE="create-test-data.generated.sql"
+
+if [ -f "$CREATE_FILE" ]
+then
+   rm "$CREATE_FILE"
+fi
+
+for tenantId in `seq 1 $TENANT_COUNT`
+do
+   KEY=`uuid`
+   PUBLIC_KEY="public-key-$KEY"
+   SHARED_SECRET="shared-secret-$KEY"
+   BASE_URL="https://$tenantId.atlassian.com"
+   PRODUCT_TYPE="JIRA"
+   echo "INSERT INTO tenant (id, key, publickey, sharedsecret, baseurl, producttype) VALUES ($tenantId, 'jira:$KEY', '$PUBLIC_KEY', '$SHARED_SECRET', '$BASE_URL', '$PRODUCT_TYPE');" >> "$CREATE_FILE"
+
+   for reminderId in `seq 1 $REMINDERS_PER_TENANT`
+   do
+      SUMMARY=`uuid`
+      USER_KEY=`uuid`
+      USER_MAIL="$USER_KEY@test.test"
+      USER_MESSAGE="User: $USER_KEY Summary: $SUMMARY"
+      HOURS_INTERVAL=`expr "$RANDOM" '-' '16383'`
+      echo "INSERT INTO ping (tenantId, issueId, issueKey, issueSummary, userKey, userEmail, message, date) VALUES ($tenantId, $RANDOM, 'ISSUE-$RANDOM', '$SUMMARY', '$USER_KEY', '$USER_MAIL', '$USER_MESSAGE', current_timestamp + interval '$HOURS_INTERVAL hours');" >> "$CREATE_FILE" 
+   done
+done
+
