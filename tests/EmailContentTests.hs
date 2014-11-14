40cfe1ae7dbd95b59cef24eeec9d0013d35e6678
Avi Knoll <aknoll@atlassian.com>
2014-11-14 11:45:16 +1100

    CHA-84 test failing usefully for reminderEmail

--- tests/EmailContentTests.hs
+++ tests/EmailContentTests.hs
@@ -2,19 +2,18 @@
 
 module EmailContentTests where
 
-import Data.Char
+import Data.Maybe (fromJust)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import Data.Text.Encoding.Error
-import Data.Time.Clock
 import Data.Time.Calendar
-import Data.Maybe (fromJust)
+import Data.Time.Clock
 import Mail.Hailgun
 import Network.URI
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
-import Test.QuickCheck.Monadic (assert, monadicIO, PropertyM)
+import Test.QuickCheck.Instances ()
 
 import EmailContent
 import Persistence.Reminder
@@ -24,19 +23,16 @@ tests =
   [ testProperty "Email message contains the input message" prop_emailMessageContentContainsMessage
   ]
 
-decode = TE.decodeUtf8With strictDecode
+decode = TE.decodeUtf8With lenientDecode
 
-prop_emailMessageContentContainsMessage :: String -> Property
-prop_emailMessageContentContainsMessage s =
-  s' /= "" ==> counterexample (T.unpack messageText) . ioProperty $ do
-    putStrLn s'
-    return . property $ remainder /= messageText
+prop_emailMessageContentContainsMessage :: T.Text -> Property
+prop_emailMessageContentContainsMessage tIn =
+  tNormalised /= "" ==> counterexample (T.unpack tNormalised ++ "\n" ++ T.unpack messageText) . property $
+    tNormalised `T.isInfixOf` messageText
   where
-    s' = filter isPrint s
+    tNormalised = decode . TE.encodeUtf8 $ tIn
     time = UTCTime (fromGregorian 2000 1 2) (secondsToDiffTime 30)
     uri = fromJust $ parseURI "http://test.com"
-    packed = T.pack s'
-    reminder = EmailReminder 1 2 "key" "key2" "stuff" "other stuff" "userkey" "email@address" (Just packed) time uri
+    reminder = EmailReminder 1 2 "key" "key2" "stuff" "other stuff" "userkey" "email@address" (Just tNormalised) time uri
     -- This is the actual conversion and extraction:
     messageText = decode $ textContent . reminderEmail $ reminder
-    (_, remainder) = T.breakOn packed messageText
