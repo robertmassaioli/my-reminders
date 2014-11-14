cf18d36ac83c7ea64b712702c2e9e7b8f569b094
Avi Knoll <aknoll@atlassian.com>
2014-11-14 15:55:10 +1100

    CHA-84 passing test for message inclusion in email

--- tests/EmailContentTests.hs
+++ tests/EmailContentTests.hs
@@ -2,18 +2,19 @@
 
 module EmailContentTests where
 
-import Data.Maybe (fromJust)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import qualified Data.Text.Encoding as TE
-import Data.Text.Encoding.Error
-import Data.Time.Calendar
-import Data.Time.Clock
+import Data.Time.Clock (UTCTime(..), secondsToDiffTime)
+import Data.Time.Calendar (fromGregorian)
+import Data.Maybe (fromJust)
 import Mail.Hailgun
-import Network.URI
+import Network.URI (parseURI)
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
 import Test.QuickCheck.Instances ()
+import Text.HTML.TagSoup.Entity (escapeXML)
 
 import EmailContent
 import Persistence.Reminder
@@ -23,16 +24,17 @@ tests =
   [ testProperty "Email message contains the input message" prop_emailMessageContentContainsMessage
   ]
 
-decode = TE.decodeUtf8With lenientDecode
+decode = TE.decodeUtf8
 
 prop_emailMessageContentContainsMessage :: T.Text -> Property
 prop_emailMessageContentContainsMessage tIn =
   tNormalised /= "" ==> counterexample (T.unpack tNormalised ++ "\n" ++ T.unpack messageText) . property $
-    tNormalised `T.isInfixOf` messageText
+    tNormalisedEscaped `T.isInfixOf` messageText
   where
     tNormalised = decode . TE.encodeUtf8 $ tIn
+    tNormalisedEscaped = T.pack . escapeXML . T.unpack $ tNormalised
     time = UTCTime (fromGregorian 2000 1 2) (secondsToDiffTime 30)
     uri = fromJust $ parseURI "http://test.com"
     reminder = EmailReminder 1 2 "key" "key2" "stuff" "other stuff" "userkey" "email@address" (Just tNormalised) time uri
     -- This is the actual conversion and extraction:
-    messageText = decode $ textContent . reminderEmail $ reminder
+    messageText = decode $ htmlContent . reminderEmail $ reminder
