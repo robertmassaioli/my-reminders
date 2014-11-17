a6701d02e791e445c39f435af98a4456647e4bda
Robert Massaioli <rmassaioli@atlassian.com>
2014-11-17 17:40:21 +1100

    CHA-74: The HTML email sends correctly.
    Still need to make the text email of the same type and then we need to make sure that the HTML email
    has the same content.

---
+++ src/Finder.hs
@@ -0,0 +1,32 @@
+module Finder
+    ( findFile
+    , findDirectory
+    , findWithFilter
+    ) where
+
+import           Control.Applicative
+import           Control.Monad       (filterM)
+import           Data.List           (inits)
+import           Data.Maybe          (listToMaybe)
+import qualified System.Directory    as SD
+import           System.Environment  (getExecutablePath)
+import           System.FilePath     (dropFileName, joinPath, splitDirectories)
+
+findFile :: (FilePath -> FilePath) -> IO (Maybe FilePath)
+findFile path = findWithFilter path SD.doesFileExist
+
+findDirectory :: (FilePath -> FilePath) -> IO (Maybe FilePath)
+findDirectory path = findWithFilter path SD.doesDirectoryExist
+
+findWithFilter :: (FilePath -> FilePath) -> (FilePath -> IO Bool) -> IO (Maybe FilePath)
+findWithFilter findPath findFilter = do
+    exeDir <- getExecutableDirectory
+    let potentialLocations = fmap findPath (getParentDirectories exeDir)
+    listToMaybe <$> filterM findFilter potentialLocations
+
+getExecutableDirectory :: IO FilePath
+getExecutableDirectory = fmap dropFileName getExecutablePath
+
+getParentDirectories :: FilePath -> [FilePath]
+getParentDirectories = reverse . fmap joinPath . tail . inits . splitDirectories
+
