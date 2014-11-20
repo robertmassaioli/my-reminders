74fef640a2e5436f9e8793b88147a63333d9f636
Robert Massaioli <rmassaioli@atlassian.com>
2014-11-21 09:04:26 +1100

    CHA-74: Fixing the test cases.

--- tests/EmailContentTests.hs
+++ tests/EmailContentTests.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 module EmailContentTests where
 
 import qualified Data.Text as T
@@ -10,9 +9,11 @@ import Data.Time.Calendar (fromGregorian)
 import Data.Maybe (fromJust)
 import Mail.Hailgun
 import Network.URI (parseURI)
+import EmailContext
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck
+import Test.QuickCheck.Monadic
 import Test.QuickCheck.Instances ()
 import Text.HTML.TagSoup.Entity (escapeXML)
 
@@ -27,14 +28,26 @@ tests =
 decode = TE.decodeUtf8
 
 prop_emailMessageContentContainsMessage :: T.Text -> Property
-prop_emailMessageContentContainsMessage tIn =
-  tNormalised /= "" ==> counterexample (T.unpack tNormalised ++ "\n" ++ T.unpack messageText) . property $
-    tNormalisedEscaped `T.isInfixOf` messageText
+prop_emailMessageContentContainsMessage tIn = monadicIO $ do
+  messageText <- run getMessageText
+  pre $ tNormalised /= ""
+  pre . and $ fmap (\x -> not $ T.pack x `T.isInfixOf` tNormalised) ["'", "\\"]
+  assert $ tNormalisedEscaped `T.isInfixOf` messageText
   where
     tNormalised = decode . TE.encodeUtf8 $ tIn
     tNormalisedEscaped = T.pack . escapeXML . T.unpack $ tNormalised
     time = UTCTime (fromGregorian 2000 1 2) (secondsToDiffTime 30)
     uri = fromJust $ parseURI "http://test.com"
     reminder = EmailReminder 1 2 "key" "key2" "stuff" "other stuff" "userkey" "email@address" (Just tNormalised) time uri
+    emailContext = EmailContext
+       { ecConnectConf = undefined
+       , ecAppConf = undefined
+       , ecPlainEmailTemplate = T.pack "{{reminderMessage}}"
+       , ecHtmlEmailTemplate = T.pack "{{reminderMessage}}"
+       , ecAttachments = []
+       }
     -- This is the actual conversion and extraction:
-    messageText = decode $ htmlContent . reminderEmail $ reminder
+    getMessageText :: IO T.Text
+    getMessageText = do
+      messageContent <- reminderEmail emailContext reminder
+      return . decode . htmlContent $ messageContent
