619f699942634b85fbe325ba9b51efc76306d9a8
Robert Massaioli <rmassaioli@atlassian.com>
2014-11-21 08:32:19 +1100

    MR-2: Direct link to get around JIRA for software issue.
    https://jdog.jira-dev.com/browse/SW-1142

--- src/Site.hs
+++ src/Site.hs
@@ -117,7 +117,8 @@ applicationRoutes =
 -- change where that points to, we only have to quickly update those links here
 redirects :: [(ByteString, SS.Handler App App ())]
 redirects =
-   [ ("/redirect/raise-issue", SC.redirect "https://ecosystem.atlassian.net/projects/MR")
+   -- Have to redirect right to the correct rapid board thanks to: https://jdog.jira-dev.com/browse/SW-1142
+   [ ("/redirect/raise-issue", SC.redirect "https://ecosystem.atlassian.net/secure/RapidBoard.jspa?projectKey=MR&rapidView=189")
    , ("/redirect/install", SC.redirect "https://marketplace.atlassian.com/plugins/com.atlassian.myreminders")
    , ("/redirect/help", SC.redirect "/docs/about")
    ]
