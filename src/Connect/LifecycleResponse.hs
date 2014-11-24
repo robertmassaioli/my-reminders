0a5c53270644c90b0c9c77b4bbd80d9557bfdd0b
Robert Massaioli <rmassaioli@atlassian.com>
2014-11-25 10:03:40 +1100

    MR-29: Forgot to add the lifecycle response.

---
+++ src/Connect/LifecycleResponse.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DeriveGeneric #-}
+module Connect.LifecycleResponse
+   ( LifecycleResponse(..)
+   , ClientKey
+   ) where
+
+import           AesonHelpers
+import           Connect.Instances
+import           Data.Aeson.Types
+import qualified Data.Text         as T
+import           GHC.Generics
+import qualified Network.URI       as NU
+
+type ClientKey = T.Text
+
+data LifecycleResponse = LifecycleResponseInstalled {
+    lrKey            :: T.Text
+  , lrClientKey      :: ClientKey
+  , lrPublicKey      :: T.Text
+  , lrSharedSecret   :: Maybe T.Text
+  , lrServerVersion  :: Maybe T.Text
+  , lrPluginsVersion :: Maybe T.Text
+  , lrBaseUrl        :: ConnectURI
+  , lrProductType    :: Maybe T.Text
+  , lrDescription    :: Maybe T.Text
+  , lrEventType      :: Maybe T.Text
+} deriving (Eq, Show, Generic)
+
+instance FromJSON LifecycleResponse where
+    parseJSON = genericParseJSON defaultOptions
+      { omitNothingFields = True
+      , fieldLabelModifier = stripFieldNamePrefix "lr"
+      }
