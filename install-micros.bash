216dc405f516a25aa6a693ec8e9048cf72b1a0b4
Robert Massaioli <rmassaioli@atlassian.com>
2015-01-09 16:00:00 +1100

    Making sure that we always get the latest micros.

--- install-micros.bash
+++ install-micros.bash
@@ -1,9 +1,12 @@
-MICROS_VERSION=${MICROS_VERSION:-1.3.0}
+EACJ_USER=${bamboo_micros_user}
+EACJ_PASSWORD=${bamboo_micros_password}
 
-wget https://s3-ap-southeast-2.amazonaws.com/atl-software/micros-cli.${MICROS_VERSION}.tar.gz
-mkdir -p micros
-tar -zxvf micros-cli.${MICROS_VERSION}.tar.gz -C ./micros
-rm -f micros-cli.${MICROS_VERSION}.tar.gz
-cd micros
-npm install --production
-chmod +x micros.js
+npm config set registry=https://npm.atlassian.io
+
+npm login --registry=https://npm-private.atlassian.io --scope=atlassian <<CREDS
+$EACJ_USER
+$EACJ_PASSWORD
+rmassaioli@atlassian.com
+CREDS
+
+npm install --production '@atlassian/micros-cli'
