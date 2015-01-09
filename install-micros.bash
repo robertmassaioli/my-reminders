2b77233b98c42091a442235826e47dbdfb649094
Robert Massaioli <rmassaioli@atlassian.com>
2015-01-10 09:20:18 +1100

    Updated the install micros script again.

--- install-micros.bash
+++ install-micros.bash
@@ -13,17 +13,21 @@ then
    exit 1
 fi
 
+echo "Install the very latest npm using npm."
+npm install npm@2
+ln -s ./node_modules/npm/bin/npm .
+
 echo "Setting the npm registry to use the atlassian registry."
-npm config set registry=https://npm.atlassian.io
+./npm config set registry=https://npm.atlassian.io
 
 echo "Logging into the NPM registry with EACJ credentials: $EACJ_USER"
-npm login --registry=https://npm-private.atlassian.io --scope=atlassian <<CREDS
+./npm login --registry=https://npm-private.atlassian.io --scope=atlassian <<CREDS
 $EACJ_USER
 $EACJ_PASSWORD
 rmassaioli@atlassian.com
 CREDS
 
 echo "Installing micros-cli"
-npm install --production '@atlassian/micros-cli'
+./npm install --production '@atlassian/micros-cli'
 
 echo "micros-cli installed"
