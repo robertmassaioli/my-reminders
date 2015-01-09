985da3a5437d72a26fab1c37ff5c810b6a92e216
Robert Massaioli <rmassaioli@atlassian.com>
2015-01-10 09:33:07 +1100

    Adding the install micros updates for the npm auth token.

--- install-micros.bash
+++ install-micros.bash
@@ -1,5 +1,6 @@
 EACJ_USER=${bamboo_micros_user}
 EACJ_PASSWORD=${bamboo_micros_password}
+NPM_AUTH_TOKEN=${bamboo_npm_auth_token}
 
 if [ "x$EACJ_USER" == "x" ]
 then
@@ -13,6 +14,12 @@ then
    exit 1
 fi
 
+if [ "x$NPM_AUTH_TOKEN" == "x" ]
+then
+   echo "Error: no NPM auth token was provided"
+   exit 1
+fi
+
 echo "Install the very latest npm using npm."
 npm install npm@2
 ln -s ./node_modules/npm/bin/npm .
@@ -21,16 +28,21 @@ echo "Setting the npm registry to use the atlassian registry."
 ./npm config set registry=https://npm.atlassian.io
 
 echo "Logging into the NPM registry with EACJ credentials: $EACJ_USER"
-./npm login --registry=https://npm-private.atlassian.io --scope=atlassian <<CREDS
-$EACJ_USER
-$EACJ_PASSWORD
-rmassaioli@atlassian.com
-CREDS
+##./npm login --registry=https://npm-private.atlassian.io --scope=atlassian <<CREDS
+##$EACJ_USER
+##$EACJ_PASSWORD
+##rmassaioli@atlassian.com
+##CREDS
+cat > ~/.npmrc <<RAWDOC
+registry=https://npm.atlassian.io/
+@atlassian:registry=https://npm-private.atlassian.io/
+//npm-private.atlassian.io/:_authToken=$NPM_AUTH_TOKEN
+RAWDOC
 
 if ! grep '//npm-private.atlassian.io/:_authToken' ~/.npmrc > /dev/null
 then
-   echo "You have failed to generate an authentication token for NPM."
-   exit 1
+   echo "Error: You have failed to generate an authentication token for NPM."
+   exit 2
 fi
 
 echo "Installing micros-cli"
