9cf02bedaa4cec372629bd472e51712a6856769e
Robert Massaioli <rmassaioli@atlassian.com>
2015-02-25 08:46:21 +1100

    Updating the check script again.

--- load-and-push-docker.bash
+++ load-and-push-docker.bash
@@ -82,8 +82,9 @@ then
    exit 1
 fi
 
-echo "Checking the docker repository to see if the push succeeded..."
-if ! curl -f -q -u "${DOCKER_USERNAME}:${DOCKER_PASSWORD}" "https://${DOCKER_REMOTE}/v1/repositories/atlassian/${SERVICE_ID}/tags/${RELEASE_VERSION}"
+CHECK_URL="https://${DOCKER_REMOTE}/v1/repositories/atlassian/${SERVICE_ID}/tags/${RELEASE_VERSION}"
+echo "Checking the docker repository to see if the push succeeded by hitting: ${CHECK_URL}"
+if ! curl -f -q -u "${DOCKER_LOGIN_USERNAME}:${DOCKER_LOGIN_PASSWORD}" "${CHECK_URL}"
 then
    echo "Error: the docker push seems to have failed because it is not present in the repository."
    exit 2
