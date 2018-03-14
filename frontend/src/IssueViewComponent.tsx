7358c078ddbda0ce5a0d1b3f7f0b8d664ad9e9ff
Robert Massaioli <rmassaioli@atlassian.com>
2018-03-15 09:42:32 +1100

    The issue reminder view is taking shape. We are getting much closer to parity.

--- frontend/src/IssueViewComponent.tsx
+++ frontend/src/IssueViewComponent.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { RouteProps } from 'react-router';
 import { PageContext } from './page-context';
+import EmptyState from '@atlaskit/empty-state';
+import { IssueView } from './IssueView';
+import { ReminderView } from './Data';
+import * as moment from 'moment';
 import 'whatwg-fetch';
 
 type IssueViewComponentProps = {
@@ -24,9 +28,12 @@ type ReminderResponse = {
 
 export class IssueViewComponent 
     extends React.PureComponent<RouteProps & IssueViewComponentProps, IssueViewComponentState> {
-    componentDidMount() {
+     
+    componentWillMount() {
         this.setState({});
+    }
 
+    componentDidMount() {
         const issue = this.props.pageContext.issue;
 
         if (issue) {
@@ -54,9 +61,41 @@ export class IssueViewComponent
         }
     }
 
+    componentDidUpdate() {
+        AP.resize();
+    }
+
     render() {
+        const reminders = this.state.reminders;
+        if (typeof reminders === 'undefined') {
+            return <p>Loading issue reminers...</p>;
+        } else if (reminders === 'reminders-failed-to-load') {
+            return (
+                <EmptyState 
+                    header="Could not load reminders"
+                    description="We could not load the reminders for this issue. Please 
+                            try to reload the page and if problems persist then get help."
+                    secondaryAction={<a href="/redirect/report-issue">Get help</a>}
+                />
+            );
+        }
+
+        const rvs: ReminderView[] = reminders.map(r => {
+            const momentDate = moment(r.Date).tz('Australia/Sydney');
+            return {
+                id: r.ReminderId,
+                message: r.Message,
+                expiresAt: momentDate.toDate(),
+                timezone: 'Australia/Sydney'
+            };
+        });
+
+        const noOp = () => {
+            // noOp
+        };
+
         return (
-            <p>{this.state && this.state.reminders ? this.state.reminders.length : 'NA'}</p>
+            <IssueView reminders={rvs} onAddReminder={noOp} onTomorrow={noOp} onInAWeek={noOp} onInAMonth={noOp} />
         );
     }
 }
