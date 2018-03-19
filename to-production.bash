eb7a7eb327b36531c065de6b7aeb7dbf4583f7dd
Robert Massaioli <rmassaioli@atlassian.com>
2018-03-19 17:46:57 +1100

    Use yarn instead of npm.

--- to-production.bash
+++ to-production.bash
@@ -33,7 +33,7 @@ cp -R static "$COPY_DIR"
 
 # Bulid Frontend Resources
 cd frontend
-npm install && npm run get-swagger-codegen && npm build
+yarn install && yarn run get-swagger-codegen && yarn build
 mkdir -p "${COPY_DIR}/frontend/build"
 cp -R build "${COPY_DIR}/frontend/build"
 cd "$DIR"
