24cf12e3339c0764bce2b58809d6dbb56db52d75
Robert Massaioli <rmassaioli@atlassian.com>
2018-03-21 09:34:35 +1100

    Remove the rm command.

--- build-production.bash
+++ build-production.bash
@@ -9,7 +9,7 @@ IMAGE_TAG=`git describe`
 DOCKER_CMD=${DOCKER_CMD:-docker}
 
 echo "## Building production image: $IMAGE_TAG"
-time ${DOCKER_CMD} build --rm="true" --tag="$IMAGE_TAG" "${DIR}"
+time ${DOCKER_CMD} build --tag="$IMAGE_TAG" "${DIR}"
 echo "## Built production image: $IMAGE_TAG"
 
 exit 0
