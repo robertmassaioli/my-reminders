50ce1fc942212e7ef1c8c37a2b4fb7b0d330cbda
Robert Massaioli <rmassaioli@atlassian.com>
2019-04-10 08:08:01 +1000

    MR-83: Catching exceptions when talking to the products.
    This is required for JDog and SDog.

--- src/AaidSlurpHandlers.hs
+++ src/AaidSlurpHandlers.hs
@@ -5,9 +5,11 @@ module AaidSlurpHandlers ( handleAaidSlurp ) where
 import           AesonHelpers
 import           Application
 import           Control.Monad                     (sequence)
+import qualified Control.Exception      as E
+import qualified Control.Exception.Lifted as EL
 import           Data.Aeson
 import           Data.Aeson.TH
-import           Data.Either                       (lefts)
+import           Data.Maybe                        (catMaybes)
 import qualified Data.Text                         as T
 import           GHC.Generics
 import qualified Model.UserDetails                 as UD
@@ -31,6 +33,14 @@ data SlurpRunResult = SlurpRunResult
 instance ToJSON SlurpRunResult where
   toJSON = genericToJSON (baseOptions { fieldLabelModifier = stripFieldNamePrefix "ssr" })
 
+-- TODO: For now we just catch everything but in the future we might choose to be more
+-- selective...maybe.
+exceptionFilter :: E.SomeException -> Maybe E.SomeException
+exceptionFilter = Just
+
+simpleCatch :: AppHandler a -> AppHandler (Either E.SomeException a)
+simpleCatch = EL.tryJust exceptionFilter
+
 -- Get all of the reminders with no aaid
 -- For the first 10, get
 handleAaidSlurpRun :: AppHandler ()
@@ -38,11 +48,30 @@ handleAaidSlurpRun = do
   reminders <- R.getRemindersMissingAaids
   -- TODO only take the first ten reminders
   let (toProcess, remainder) = splitAt 50 reminders
-  responses <- sequence . fmap updateReminder $ toProcess
-  let errors = fmap (AC.perMessage) . lefts $ responses
+  responses <- sequence . fmap safeUpdateReminder $ toProcess
+  let errors = getErrors responses
   let results = SlurpRunResult errors (length toProcess) (length remainder)
   SH.writeJson results
   SH.respondWith SH.ok
+
+data UpdateReminderError
+  = ProductErrorResponse AC.ProductErrorResponse
+  | ExceptionResult E.SomeException
+
+getErrors :: [Maybe UpdateReminderError] -> [T.Text]
+getErrors = fmap toErrorMessage . catMaybes
+
+toErrorMessage :: UpdateReminderError -> T.Text
+toErrorMessasge (ProductErrorResponse e) = "Product Error Response: " `T.append` AC.perMessage e
+toErrorMessage (ExceptionResult e) = "Exception: " `T.append` (T.pack . show $ e)
+
+safeUpdateReminder :: (R.Reminder, AC.Tenant) -> AppHandler (Maybe UpdateReminderError)
+safeUpdateReminder input = do
+  result <- simpleCatch $ updateReminder input
+  case result of
+    Left e -> return . Just . ExceptionResult $ e
+    Right (Left e) -> return . Just . ProductErrorResponse $ e
+    _ -> return Nothing
 
 updateReminder :: (R.Reminder, AC.Tenant) -> AppHandler (Either AC.ProductErrorResponse ())
 updateReminder (reminder, tenant) = do
