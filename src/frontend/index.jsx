9fa01bb7750006b4e5fd38b4ccd77512ce71a876
Robert Massaioli <rmassaioli@atlassian.com>
2023-11-14 16:46:20 +1100

    Got much of the reminder creation and deletion code working.
    Still working on other parts of the app.

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,71 +1,138 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text, Heading, ButtonSet, Button, Table, Head, Cell, Row, Tooltip, Link, ModalDialog } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import ForgeReconciler, { Text, Heading, ButtonSet, Button, Table, Head, Cell, Row, Tooltip, Link, ModalDialog, SectionMessage } from '@forge/react';
+import { invoke, requestJira, view } from '@forge/bridge';
+import { useEffectAsync } from '../useEffectAsync';
+import moment from 'moment-timezone';
+import { isPresent } from 'ts-is-present';
+
+async function createReminder({ issueSummary, timestamp, message }) {
+  const response = await invoke('createReminder', {
+    issueSummary,
+    timestamp,
+    message
+  });
+
+  return response;
+}
+
+async function getIssueSummary(context) {
+  const response = await requestJira(`/rest/api/3/issue/${context.extension.issue.id}`, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  const issueData = await response.json();
+  return issueData.fields.summary;
+}
+
+async function getUserTimezone(context) {
+  const response = await requestJira(`/rest/api/3/user?accountId=${context.accountId}`, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  const data = await response.json();
+  return data.timezone;
+}
 
 const App = () => {
   const [isAddReminderOpen, setAddReminderOpen] = useState(false);
   const [reminders, setReminders] = useState(undefined);
-  //const [data, setData] = useState(null);
+  const [userTimezone, setUserTimezone] = useState(undefined);
 
-  useEffect(() => {
-    if (reminders === undefined) {
-      invoke('getMyReminders').then(setReminders);
-    }
-  });
-  // useEffect(() => {
-  //   invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  // }, []);
+  useEffectAsync(async () => {
+    const data = await invoke('getMyReminders');
+    setReminders(data.reminders);
+  }, reminders);
+
+  useEffectAsync(async () => {
+    const context = await view.getContext();
+    const data = await getUserTimezone(context);
+    setUserTimezone(data.timeZone);
+  }, userTimezone);
+
+  if (isPresent(userTimezone)) {
+    moment.tz.setDefault(userTimezone);
+  }
+
+  async function createReminderForTomorrow() {
+    const context = await view.getContext();
+    const issueSummary = await getIssueSummary(context);
+
+    // TODO Randomise between 6 and 8 am
+    const tomorrow = moment().add(1, 'day').set('hour', 7);
+
+    const response = await createReminder({
+      issueSummary,
+      timestamp: tomorrow.unix(),
+      message: undefined
+    });
+
+    setReminders(response.reminders);
+  }
+
+  async function deleteReminder(reminderKey) {
+    const data = await invoke('deleteReminder', { reminderKey });
+
+    console.log(JSON.stringify(data.reminders, null, 2));
+    setReminders(data.reminders);
+  }
 
   return (
     <>
       <Heading size="medium">Add reminder</Heading>
       <ButtonSet>
-        <Button text="Tomorrow" onClick={async () => setReminders(await createReminderForTomorrow(viewContext))} />
-        <Button text="In a Week" />
-        <Button text="Select a time..." onClick={() => setAddReminderOpen(true)}/>
+        <Button onClick={() => createReminderForTomorrow()}>Tomorrow</Button>
+        <Button>In a Week</Button>
+        <Button onClick={() => setAddReminderOpen(true)}>Select a time...</Button>
       </ButtonSet>
-      <Heading size="medium">Your reminders</Heading>
       <Text>Reminder Data: {JSON.stringify(reminders, null, 2)}</Text>
-      {reminders && (
-        <Table>
-        <Head>
-          <Cell><Text>When</Text></Cell>
-          <Cell><Text>Message</Text></Cell>
-          <Cell><Text>Action</Text></Cell>
-        </Head>
-        <Row>
-          <Cell>
-            <Tooltip text="15 Nov 2023 at 7AM"><Text>In 5 days</Text></Tooltip>
-          </Cell>
-          <Cell>
-            <Text>Make sure that we say something meaningful here</Text>
-          </Cell>
-          <Cell><Button icon="editor-remove" /></Cell>
-        </Row>
-        <Row>
-          <Cell>
-            <Tooltip text="15 Nov 2023 at 7AM"><Text>In 5 days</Text></Tooltip>
-          </Cell>
-          <Cell>
-            <Text>(No Message)</Text>
-          </Cell>
-          <Cell><Button icon="editor-remove" /></Cell>
-        </Row>
-        <Row>
-          <Cell>
-            <Tooltip text="15 Nov 2023 at 7AM"><Text>In 5 days</Text></Tooltip>
-          </Cell>
-          <Cell>
-            <Text>Short message here</Text>
-          </Cell>
-          <Cell><Button icon="editor-remove" /></Cell>
-        </Row>
-      </Table>
+      {/* <Text>Issue Summary: {issueSummary}</Text> */}
+      {reminders && reminders.length > 0 && (
+        <>
+          <Heading size="medium">Your reminders</Heading>
+          <Table>
+            <Head>
+              <Cell><Text>When</Text></Cell>
+              <Cell><Text>Message</Text></Cell>
+              <Cell><Text>Action</Text></Cell>
+            </Head>
+            {reminders.map(reminder => {
+              const { date, message } = reminder.value;
+              const expiry = moment.unix(date);
+              const dateOutput = expiry.format('D MMM YYYY');
+              const hourOutput = expiry.format('h');
+              const meridian = expiry.format('A');
+              expiry.add(1, 'hour');
+              const nextHourOutput = expiry.format('h');
+
+              const fullDateOutput = `${dateOutput} at ${hourOutput}-${nextHourOutput}${meridian}`;
+              return (
+                <Row>
+                  <Cell>
+                    <Tooltip text={fullDateOutput}><Text>{expiry.fromNow()}</Text></Tooltip>
+                  </Cell>
+                  <Cell>
+                    <Text>{message || '<No Message>'}</Text>
+                  </Cell>
+                  <Cell><Button icon="editor-remove" onClick={() => deleteReminder(reminder.key)} /></Cell>
+                </Row>
+              );
+            })}
+          </Table>
+        </>
+      )}
+      {reminders && reminders.length === 0 && (
+        <SectionMessage>
+          <Text>You have no reminders on this issue. Create some!</Text>
+        </SectionMessage>
       )}
       {reminders === undefined && (
-        <Text>Loading your reminders...</Text>
+        <SectionMessage>
+          <Text>Loading your reminders...</Text>
+        </SectionMessage>
       )}
-      <Text>Your timezone: <Link href="https://rmassaioli-development.atlassian.net/secure/ViewPersonalSettings.jspa">Sydney/Australia</Link></Text>
+      <Text>Your timezone: <Link href="/secure/ViewPersonalSettings.jspa">{userTimezone || "Loading..."}</Link></Text>
       {isAddReminderOpen && (
         <ModalDialog header="Add a Reminder" onClose={() => setAddReminderOpen(false)}>
           <Text>Hello!</Text>
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,8 @@
 import Resolver from '@forge/resolver';
 import api, { storage } from "@forge/api";
+import moment from 'moment-timezone';
+import { isPresent } from 'ts-is-present';
+import { view } from '@forge/bridge';
 
 const resolver = new Resolver();
 
@@ -36,29 +39,92 @@ resolver.define('getText', (req) => {
   }
 }
 */
-resolver.define('getMyReminders', async (req) => {
-  const newResult = await storage
-    .entity('reminder')
-    .query()
-    .index('by-key')
-    .getMany();
-
-  console.log(JSON.stringify(newResult, null, 2))
-
-  const viewContext = {
+function extractViewContext(req) {
+  return {
     userAaid: req.context.accountId,
-    issueId: req.context.extension.issue.id
+    issueId: parseInt(req.context.extension.issue.id),
+    issueKey: req.context.extension.issue.key
   };
-  console.log(JSON.stringify(viewContext));
+}
+
+async function getUserTimezone(accountId) {
+  const response = await api.asUser().requestJira(route`/rest/api/3/user?accountId=${accountId}`, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  const data =  await response.json();
+
+  return data.timeZone;
+}
+
+async function getReminders(viewContext) {
   const result = await storage
     .entity('reminder')
     .query()
-    .index("by-aaid-and-issue-id-v2", {
+    .index("by-aaid-and-issue-id", {
       partition: [viewContext.userAaid, viewContext.issueId]
     })
     .getMany();
 
   return result.results;
+}
+
+resolver.define('getMyReminders', async (req) => {
+  return {
+    reminders: await getReminders(extractViewContext(req))
+  };
+});
+
+resolver.define('createReminder', async (req) => {
+  const viewContext = extractViewContext(req);
+
+  const { timestamp, issueSummary, message } = req.payload;
+
+  const expiryTime = moment.unix(timestamp);
+
+  const entityKey = `${viewContext.issueId}-${expiryTime.unix()}`;
+  await storage.entity('reminder').set(entityKey, {
+    issueId: viewContext.issueId,
+    issueKey: viewContext.issueKey,
+    originalIssueKey: viewContext.issueKey,
+    issueSummary: issueSummary,
+    originalIssueSummary: issueSummary,
+    // TODO make sure that the message is no more than X characters long.
+    message: isPresent(message) ? message : undefined,
+    date: expiryTime.unix(),
+    day: expiryTime.format('YYYY-MM-DD'),
+    userAaid: viewContext.userAaid,
+    sendAttempts: 0
+  });
+
+  return {
+    reminders: await getReminders(viewContext)
+  }
+});
+
+resolver.define('deleteReminder', async (req) => {
+  const { reminderKey } = req.payload;
+  const viewContext = extractViewContext(req);
+
+  const reminder = await storage.entity('reminder').get(reminderKey);
+  console.log(JSON.stringify(reminder));
+  console.log(`ViewContext: ${JSON.stringify(viewContext)}`)
+  if (isPresent(reminder)) {
+    // Only if the current user owns the reminder should it be able to delete it
+    if (reminder.userAaid === viewContext.userAaid) {
+      await storage.entity('reminder').delete(reminderKey);
+    } else {
+      console.log(`SECURITY ALERT: User ${viewContext.userAaid} tried to delete reminder for ${reminder.userAaid} with key ${reminderKey}`);
+    }
+  } else {
+    console.log(`Tried to delete a reminder that no longer exists.`)
+  }
+
+  return {
+    reminders: await getReminders(viewContext)
+  }
 });
 
 export const handler = resolver.getDefinitions();
