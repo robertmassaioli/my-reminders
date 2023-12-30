709452b75f474121e2d8a14371426fe087e53ca8
Robert Massaioli <rmassaioli@atlassian.com>
2023-12-30 11:48:26 +1100

    VULN-1134950: Remove unused code.
    This code is no longer required for the My Reminders App to work.

--- src/index.jsx
+++ src/index.jsx
@@ -4,39 +4,9 @@ export { sendRemindersHandler } from './resolvers/sendReminders';
 export { yourRemindersHandler } from './resolvers/yourReminders';
 //TODO Old file for UI Kit 1
 
-import api, { WhereConditions, route, storage } from "@forge/api";
-import ForgeUI, { render, Text, Fragment, GlobalPage, useState, IssueContext, ButtonSet, Button, Heading, Table, Head, Row, Cell, Link, DateLozenge, ModalDialog, Tooltip, useProductContext} from '@forge/ui';
+import { WhereConditions, storage } from "@forge/api";
 import moment from 'moment';
 import { Queue } from '@forge/events';
-
-const fetchProjects = async () => {
-  const res = await api.asApp().requestJira(route`/rest/api/3/project`, {
-    headers: {
-      'Content-type': 'application/json',
-      'Accept': 'application/json'
-    }
-  });
-
-  const data = await res.json();
-  return data;
-};
-
-const App = () => {
-  const [projects] = useState(async () => await fetchProjects());
-
-  return (
-    <Fragment>
-      <Text>Hello world! This is a General Page in the My Reminders App. This is Forge UI. It works! Boom. A Connect on Forge App!</Text>
-      {projects.map(project => (<Text>{project.name}</Text>))}
-    </Fragment>
-  );
-};
-
-export const run = render(
-  <GlobalPage>
-    <App/>
-  </GlobalPage>
-);
 
 // Why does my webtrigger return HTTP 424: what dependency am I missing?
 export async function scheduleExpiryJobs() {
